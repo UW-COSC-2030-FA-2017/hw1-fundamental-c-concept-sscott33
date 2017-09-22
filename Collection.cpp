74112d9560c36fcc1de3be04e42c7121c2f3c595
sscott33 <sscott33@uwyo.edu>
2017-09-22 17:11:29 -0600

    first save, going to dinner

---
+++ Collection.cpp
@@ -0,0 +1,15 @@
+// function definitions for the Collection Class
+// // Created by Samuel Scott on 9/22/2017 @ 16:37
+
+#include "Collection.h"
+
+Collection::Collection(int maxSize) {
+    this->maxSize = maxSize;
+    size = 0;
+    items[maxSize];
+}
+
+// unncomment main() to check for syntax errors when compiling this file
+// int main() {
+//     return 0;
+// }
---
+++ Collection.h
@@ -0,0 +1,34 @@
+// Collection class
+// Created by Samuel Scott on 9/22/2017 @ 16:32
+
+
+#ifndef _Collection_H
+#define _Collection_H
+
+#include <iostream>
+
+using namespace std;
+
+class Collection {
+
+public:
+
+    Collection(int maxSize);
+
+
+protected:
+    /** trait and collection variables **/
+    int maxSize;
+    int size;
+
+    double items[];
+
+    /** Accessors **/
+    
+
+    /** Mutators **/
+
+};
+
+
+#endif
