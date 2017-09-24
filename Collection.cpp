04801f87f3c4f24b935a045ccada4f404baeb2fe
Mike Borowczak <mike.borowczak@uwyo.edu>
2017-09-24 11:43:42 -0600

    Minor changes to get you on the right path
    Moved .h into CPP (dealing with templated classes!), slight changes to initialization in constructor, and destructor!

--- Collection.cpp
+++ Collection.cpp
@@ -1,7 +1,84 @@
 // function definitions for the Collection Class
 // // Created by Samuel Scott on 9/22/2017 @ 16:37
 
-#include "Collection.h"
+// Collection class
+// Created by Samuel Scott on 9/22/2017 @ 16:32
+
+
+#ifndef _Collection_H_
+#define _Collection_H_
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+template <class T> class Collection {
+
+public:
+
+    Collection(int maxSize);
+    ~Collection();
+
+
+    /** Accessors **/
+
+    bool isEmpty();
+        // returns true IFF there are no objects in the array "items"
+        // requires that an empty object accept null in equals()
+        // requires that an empty object be equivalent to its default
+
+    bool notContained(T x);
+        // returns true IFF the object "x" is not contained in "items"
+
+    /** Mutators **/
+
+    void makeEmpty();
+        // removes all objects from the array
+
+    bool insert(T x);
+        // inserts an object "x" as the next abailable array element
+        // returns true if there is room and insertion was successful
+        // returns false if there is no room in the array and does not...
+            // attempt to insert the object
+
+    void remove(T x);
+        // removes an object "x" from the array if it exists as an element
+        // if multiple "x" equivalents exist, it will remove them too
+
+    void removeRandom();
+        // remeoves an object at a random location in the array
+        // the indexes that can be chosen are elements of [0,size - 1]
+
+
+protected:
+    void clean();
+    // moves all objects in the array as forward as possible
+    // also removes gaps in the array
+    // should be used after each removal mutator function except makeEmpty
+    // also updates the size attribue after removals
+
+
+    // overload the assignment operator
+    // template <class T>
+    // T& operator= const T &obj;
+    T& operator=(const T& obj);
+
+
+    /** trait and collection variables **/
+    int maxSize;
+    int size;
+
+
+    T *items;
+
+
+
+
+};
+
+
+#endif
 
 //public:
 
@@ -9,9 +86,8 @@
     Collection<T>::Collection(int maxSize) {
         this->maxSize = maxSize;
         size = 0;
-        for (int i = 0; i < maxSize; i++) {
-            items[i] = new T();     // does not work this nicely in c++
-        }
+
+        items = new T[maxSize];
     }
 
     template <class T>
